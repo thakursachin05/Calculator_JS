50931e6f565a8ca36f9b387072ea9f2366cc3e4c
Devendra Singh Rana <sachinrajput10101010@gmail.com>
2022-06-24 23:16:58 +0530

    Create script.js

---
+++ Code/script.js
@@ -0,0 +1,70 @@
+// console.log(eval('3*5'));
+
+// // eval function
+
+
+// var op1  = 4;
+// var op2  = 4;
+// var operator = '*';
+
+// var result = eval(op1 + " " + operator + " " + op2);
+// console.log(result);
+
+
+var buttons = document.getElementsByClassName("button");
+var display = document.getElementById("display");
+
+// display.textContent = 0
+
+var op1 = 0;
+var op2 = null;
+var operator = null;
+
+function isOperator(value){
+    return value == '+' || value == '-' || value == '*' || value == '/';
+}
+
+for(var i = 0;i<buttons.length;i++){
+    buttons[i].addEventListener('click',function() {
+        var value = this.getAttribute('data-value');
+        var text = display.textContent.trim();
+
+        if(isOperator(value)){
+            operator = value;
+            operand1 = parseFloat(text);
+            display.textContent = "";
+        }
+        else if(value == "ac"){
+            display.textContent = "";
+        }
+        else if(value == "sign"){
+            operand1 = parseFloat(text);
+            operand1 = -1 * operand1;
+            display.textContent = operand1;
+        }
+        else if(value == "."){
+            if(text.length && !text.includes('.')){
+                display.textContent = text + '.';
+            }
+        }
+        else if(value == "%"){
+            operand1 = parseFloat(text);
+            operand1 = operand1/100;
+            display.textContent = operand1;
+        }
+        else if(value == "="){
+            operand2 = parseFloat(text);
+            var result = eval(operand1 + ' ' + operator + ' ' + operand2);
+            if (result) {
+                display.textContent = result;
+                operand1 = result;
+                operand2 = null;
+                operator = null;
+            }
+           
+        }
+        else{
+            display.textContent += value;
+        }
+    });
+}
